d44bd3b111c8e7308c2d6fac039bdb42d6c15981
AquaMedicalruh <127622705+AquaMedicalruh@users.noreply.github.com>
2023-03-11 19:16:08 +0100

    Add files via upload

---
+++ word.cpp
@@ -0,0 +1,21 @@
+#include <Windows.h>
+#include <atlbase.h>
+#include <atlcom.h>
+#include <atlstr.h>
+#include <oleacc.h>
+#include <Shlwapi.h>
+#include <msclr\marshal_cppstd.h>
+#include <comdef.h>
+#include <office.h>
+#include <iostream>
+
+int main()
+{
+    CoInitialize(NULL);
+    _ApplicationPtr pApp("Word.Application");
+    HWND hWnd = (HWND)pApp->hWnd;
+    HWND hRibbon = FindWindowEx(hWnd, NULL, L"Ribbon", NULL);
+    SetWindowPos(hRibbon, NULL, 0, 0, 500, 100, SWP_NOMOVE);
+    CoUninitialize();
+    return 0;
+}
